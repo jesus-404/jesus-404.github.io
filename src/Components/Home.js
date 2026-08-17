80fe1eb5b8610703bada35a50a4393fd4c11c1cd
Jesus aguayo <aguayo.jesus2003@gmail.com>
2026-08-16 17:11:50 -0700

    Updated Home.js to include a greeting message before the name.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -110,7 +110,7 @@ export default function Home({ isDark, opacityStyle }) {
             <div className="home-container1">
                 <div className="home-container2">
                     <div className="home-text">
-                        <p className="home-eyebrow">Hello, my name is</p>
+                        <p className="home-eyebrow">Hello, my name is:</p>
                         <h1 id="home-title">Jesus Aguayo</h1>
                         <p className="home-role">An aspiring Network Engineer</p>
                         <a className="home-btn" href="#exp">
